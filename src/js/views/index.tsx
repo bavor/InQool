83f757bc2397c9a0fd4747d68c143e0d94a9da0a
Zdendoslaf <schor.zdenek@gmail.com>
2020-08-04 21:38:08 +0200

    / repos line issue

--- src/js/views/index.tsx
+++ src/js/views/index.tsx
@@ -2,9 +2,14 @@ import React from 'react';
 import './index.sass'
 
 const Index = () => (
-    <div className="container">
-      <h1 className="text-center my-3">InQool App</h1>
-      <div className="Search col-md-10 offset-md-1 col-12 ">
+  <div className="container">
+    <div className="row">
+
+      <div className="col-12 col-md-10 offset-md-1">
+        <h1 className="text-center my-3">InQool App</h1>
+      </div>
+
+      <div className="col-12 col-md-10 offset-md-1">
         <div className="input-group mb-3 mt-3 ">
           <input type="text" className="form-control border border-success" placeholder="Enter Github username" aria-label="Recipient's username" aria-describedby="basic-addon2"/>
           <div className="input-group-append">
@@ -13,32 +18,34 @@ const Index = () => (
         </div>
       </div>
 
-      <h2 className="my-4">Repositories</h2>
-      <div className="Repositories row border views col-md-10 offset-md-1 col-12">
-        <div className="col-6 views-username">
-        <p>prvni username</p>
-        </div>
-        <div className="col-2"> 
-        <img src="https://www.mojeparty.cz/data/USR_045_DEFAULT/Balonek_foliovy_Duhovy_jednorozec_1ks_303129901.jpg"
-         alt="pony" className="img-fluid views-image" />
-        </div>
-        <div className="col-1 views-username">
-          hvezda
-        </div>
-        <div className="col-1 views-username">
-          score
-        </div>
-
+      <div className="col-12 col-md-10 offset-md-1">
+          <h2 className="my-4">Repositories</h2>
       </div>
 
+      <div className="col-12 col-md-10 offset-md-1">
+        <div className="border views-username ">
+          
+            <img src="https://www.mojeparty.cz/data/USR_045_DEFAULT/Balonek_foliovy_Duhovy_jednorozec_1ks_303129901.jpg"
+              alt="pony" className="img-fluid views-image" />
+            <p className="col-1">dfasdf</p>
+            <i className="fa fa-star-o" aria-hidden="true"></i>
+            5.0
+        </div>
+      </div>
+
+      <div className="col-12 col-md-10 offset-md-1">
       <h2 className="my-4">Organizations</h2>
-      <div className="Organizations row border">
-        <div className="col-12">
-        <h2>organizace</h2>
+      </div>
+
+      <div className="col-12 col-md-10 offset-md-1">
+        <div className="Organizations row border">
+          <p>organizace</p>
         </div>
       </div>
 
+    
     </div>
+  </div>
 )
 
 export default Index
