c3c9ba0719701a80dcc5cf2e8351672215e09692
Zdendoslaf <schor.zdenek@gmail.com>
2020-08-04 16:12:48 +0200

    +sass added

--- src/js/App.tsx
+++ src/js/App.tsx
@@ -1,17 +1,16 @@
 import React from 'react';
-import logo from '../img/logo.svg';
-import '../css/App.css';
+import '../css/App.sass';
 
 
 const App = () => (
 
-    <div className="main">
+    <div className="container">
 
       <div className="Search">
-        <div className="input-group mb-3">
-          <input type="text" className="form-control" placeholder="Enter Github username" aria-label="Recipient's username" aria-describedby="basic-addon2"/>
+        <div className="input-group mb-3 mt-3 ">
+          <input type="text" className="form-control border border-success" placeholder="Enter Github username" aria-label="Recipient's username" aria-describedby="basic-addon2"/>
           <div className="input-group-append">
-            <button type="button" className="btn btn-success">Submit</button>
+            <button type="button" className="btn btn-success border border-success">Submit</button>
           </div>
         </div>
       </div>
